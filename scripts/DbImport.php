193d7e7172488614087338b5e7cd19fe2e535ef6
Jose Antonio Guerra <jaguerra@icti.es>
2013-06-28 19:45:27 +0200

    [BUGFIX] DBImport helper not working...

--- scripts/DbImport.php
+++ scripts/DbImport.php
@@ -6,7 +6,7 @@ $databaseDumpFile = $argv[1];
 if (file_exists($databaseCredentialsFile) && file_exists($databaseDumpFile)) {
 	require_once ($databaseCredentialsFile);
 
-	$cmd = 'cat '.$databaseDumpFile.' > mysql '.$GLOBALS['TYPO3_CONF_VARS']['DB']['database'].' -u'.$GLOBALS['TYPO3_CONF_VARS']['DB']['username'].' -p'.$GLOBALS['TYPO3_CONF_VARS']['DB']['password'].' -h'.$GLOBALS['TYPO3_CONF_VARS']['DB']['host'];
+	$cmd = 'cat '.$databaseDumpFile.' | mysql '.$GLOBALS['TYPO3_CONF_VARS']['DB']['database'].' -u'.$GLOBALS['TYPO3_CONF_VARS']['DB']['username'].' -p'.$GLOBALS['TYPO3_CONF_VARS']['DB']['password'].' -h'.$GLOBALS['TYPO3_CONF_VARS']['DB']['host'];
 
 	echo $cmd;
 	return system($cmd);
