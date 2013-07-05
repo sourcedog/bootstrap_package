e633b6e865b8b2aa028913e546e7f5f17a18ff11
Jose Antonio Guerra <jaguerra@icti.es>
2013-07-05 12:02:17 +0200

    Added powermail

---
+++ htdocs/typo3conf/ext/powermail/ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "powermail".
+ *
+ * Auto generated 12-06-2013 14:00
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 'powermail',
+	'description' => 'Powermail is a well-known, powerful and easy to use mailform extension with a lots of features (spam prevention, marketing, double-optin, etc...)',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '2.0.7',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => 'uploads/tx_powermail',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Powermail dev team',
+	'author_email' => 'alexander.kellner@in2code.de',
+	'author_company' => 'in2code.de',
+	'CGLcompliance' => NULL,
+	'CGLcompliance_note' => NULL,
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
+			'typo3' => '4.6.0-7.0.0',
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+		),
+		'conflicts' => '',
+		'suggests' => 
+		array (
+		),
+	),
+);
+
+?>
